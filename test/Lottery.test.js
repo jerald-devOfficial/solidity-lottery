b647beb56f033168c49a151c1e88170a9c560328
jerald-devOfficial <spaueOfficial@gmail.com>
2024-10-28 12:11:30 +0800

    feat: add a deploy test, add test mocha script

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -16,3 +16,10 @@ beforeEach(async () => {
     .deploy({ data: bytecode })
     .send({ gas: "1000000", from: accounts[0] });
 });
+
+
+describe("Lottery", () => {
+  it("should deploy the contract", () => {
+    assert.ok(lottery.options.address);
+  });
+});
