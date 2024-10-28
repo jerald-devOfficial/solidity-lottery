4f04afa5978d3d997c61c95d59a40be140c40ae1
jerald-devOfficial <spaueOfficial@gmail.com>
2024-10-28 14:10:35 +0800

    feat: Add more tests for Lottery contract
    - Ensure a minimum amount of ether is required to enter the lottery.
    - Verify only the manager can pick the winner.
    - Test that the winner receives the prize and players are reset.

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -57,4 +57,43 @@ describe("Lottery Contract", async () => {
     assert.equal(accounts[2], players[2]);
     assert.equal(players.length, 3);
   });
+
+  it("requires a minimum amount of ether to enter", async () => {
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: 0,
+      });
+
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it("only the manager can pick the winner", async () => {
+    try {
+      await lottery.methods.pickWinner().send({ from: accounts[1] });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it("sends the money to the winner and resets the players", async () => {
+    await lottery.methods
+      .enter()
+      .send({ from: accounts[0], value: web3.utils.toWei("2", "ether") });
+
+    const initialBalance = await web3.eth.getBalance(accounts[0]);
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0],
+    });
+
+    const finalBalance = await web3.eth.getBalance(accounts[0]);
+
+    const difference = finalBalance - initialBalance;
+    assert(difference > web3.utils.toWei("1.8", "ether"));
+  });
 });
