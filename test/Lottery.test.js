4b0b52679086ad392e4ab30eb1d40ad84fa99301
jerald-devOfficial <spaueOfficial@gmail.com>
2024-10-28 12:19:50 +0800

    feat(Lottery.test.js): add test for allowing single and multiple users to enter lottery

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -17,9 +17,44 @@ beforeEach(async () => {
     .send({ gas: "1000000", from: accounts[0] });
 });
 
-
-describe("Lottery", () => {
+describe("Lottery Contract", async () => {
   it("should deploy the contract", () => {
     assert.ok(lottery.options.address);
+  });
+
+  it("should allow a user to enter the lottery", async () => {
+    await lottery.methods
+      .enter()
+      .send({ from: accounts[0], value: web3.utils.toWei("0.02", "ether") });
+
+    const players = await lottery.methods
+      .getPlayers()
+      .call({ from: accounts[0] });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(players.length, 1);
+  });
+
+  it("should allow multiple users to enter the lottery", async () => {
+    await lottery.methods
+      .enter()
+      .send({ from: accounts[0], value: web3.utils.toWei("0.02", "ether") });
+
+    await lottery.methods
+      .enter()
+      .send({ from: accounts[1], value: web3.utils.toWei("0.02", "ether") });
+
+    await lottery.methods
+      .enter()
+      .send({ from: accounts[2], value: web3.utils.toWei("0.02", "ether") });
+
+    const players = await lottery.methods
+      .getPlayers()
+      .call({ from: accounts[0] });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(accounts[1], players[1]);
+    assert.equal(accounts[2], players[2]);
+    assert.equal(players.length, 3);
   });
 });
